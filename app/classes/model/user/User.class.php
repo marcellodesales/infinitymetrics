3a4e1bcef7d6678b8ae30e03e0473027e571b242
Marcello de Sales <marcello.desales@gmail.com>
2008-11-03 05:54:54 +0000

    [Issue 57]
    - User.class.php: this is the implementation of the user class, and this will be used on the other components that requires the User as an interface. Note that this is just a draft implementation, and needs to follow the requirements.

---
+++ app/classes/model/user/User.class.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * $Id: User.class.php 202 2008-11-02 21:31:40Z marcellosales $
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITYs, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the Berkeley Software Distribution (BSD).
+ * For more information please see <http://ppm-8.dev.java.net>.
+ */
+
+/**
+ * Basic user class for the metrics workspace. User has username, password from 
+ * Java.net.
+ * 
+ * @author Marcello de Sales <marcello.sales@gmail.com>
+ * @version $Id$
+ * @package model.user
+ */
+class User {
+
+    /**
+     * It's the name of the user.
+     * @var RssItem instance
+     */
+    private $firstName;
+
+    /**
+     * Constructs a new user with a name {@link $rssItem}
+     * @var firstName this is the first name.
+     */
+    public function  __construct($firstName) {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * Sets the value of the title tag
+     * @param string $title
+     * @return the instance of this builder.
+     */
+    public function getName() {
+        return $this->firstName;
+    }
+}
+?>
