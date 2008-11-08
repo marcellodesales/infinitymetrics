9deae3933124e0720315e752c40a2770b23f31c6
Marilyne Mendolla <marilynedozois@gmail.com>
2008-11-08 21:45:15 +0000

    [Issue 105]
    Committing successful unit tests for EventCategoryTest.class.php and EventTest.class.php.

---
+++ app/classes/infinitymetrics/tests/unit/Report/EventCategoryTest.class.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once('PHPUnit/Framework.php');
+require_once('infinitymetrics/model/report/EventCategory.class.php');
+
+/**
+ * Description of EventCategoryTest
+ *
+ * @author Marilyne Mendolla and Andres Ardila
+ */
+
+class EventCategoryTest extends PHPUnit_Framework_TestCase {
+
+    protected $eventCategory;
+
+    public function setUp() {
+        $this->eventCategory = new EventCategory();
+    }
+
+    public function testMethods() {
+        $this->eventCategory->commit();
+        $this->assertEquals('COMMIT', $this->eventCategory->getEventCategory(), "Commit was not set");
+
+        $this->eventCategory->document();
+        $this->assertEquals('DOCUMENT', $this->eventCategory->getEventCategory(), "Document was not set");
+
+        $this->eventCategory->forum();
+        $this->assertEquals('FORUM', $this->eventCategory->getEventCategory(), "Forum was not set");
+
+        $this->eventCategory->issue();
+        $this->assertEquals('ISSUE', $this->eventCategory->getEventCategory(), "Issue was not set");
+
+        $this->eventCategory->mailingList();
+        $this->assertEquals('MAILING_LIST', $this->eventCategory->getEventCategory(), "Mailing List was not set");
+        
+  }
+}
+?>
---
+++ app/classes/infinitymetrics/tests/unit/Report/EventTest.class.php
@@ -0,0 +1,44 @@
+<?php
+require_once('PHPUnit/Framework.php');
+require_once('infinitymetrics/model/report/Event.class.php');
+require_once('infinitymetrics/model/user/User.class.php');
+
+
+/**
+ * Description of EventTestclass
+ *
+ * @author Andres Ardila and Marilyne Mendolla
+ */
+class EventTest extends PHPUnit_Framework_TestCase {
+
+   protected $event;
+
+   public function setUp() {
+       $this->event = new Event();
+   }
+
+   public function builderTest() {
+       $user = new User("Andres");
+       $date = new DateTime("2008-11-08");
+
+       $this->event->builder($user, $date);
+       $this->assertEquals($user, $this->event->getUser(), "User is not equal");
+       $this->assertEquals($date, $this->event->getDateObject(), "Date is not equal");
+
+   }
+
+   public function testGetsSets() {
+
+       $user = new User("Marilyne");
+       $this->event->setUser($user);
+       $this->assertEquals($user, $this->event->getUser(), "User is not equal");
+
+       $date = new DateTime("2009-01-25");
+       $this->event->setDate($date);
+       $this->assertEquals($date, $this->event->getDateObject(), "Date is not equal");
+
+       $this->assertEquals($date->format("Y-m-d"), $this->event->getDateString(), "Date string is not equal");
+
+   }
+}
+?>
