a0b5c38a7dfdd20772b6599e9acc2f3e6b48b0ce
Andres Ardila <aardilam@gmail.com>
2008-11-14 23:15:44 +0000

    [Issue 49, Issue 112]
    Tested UC103 successfully.

---
+++ app/classes/infinitymetrics/tests/functional/workspace/UC103Test.class.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once('PHPUnit/Framework.php');
+require_once('infinitymetrics/controller/MetricsWorkspaceController.php');
+
+/**
+ * Description of UC103Test
+ *
+ * @author Andres Ardila
+ */
+
+class UC103Test extends PHPUnit_Framework_TestCase
+{
+    private $ws;
+
+    const WS_ID = 4;
+    const TITLE = "Updated Title";
+    const DESCRIPTION = "Updated Description";
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->ws = WorkspacePeer::retrieveByPK(self::WS_ID);
+    }
+
+    public function testUpdateProfile() {
+        try {
+            $this->ws = MetricsWorkspaceController::UpdateWorkspaceProfile(
+                self::WS_ID, self::TITLE, self::DESCRIPTION
+            );
+
+            $this->assertNotNull($this->ws);
+            $this->assertTrue($this->ws instanceof Workspace);
+            $this->assertEquals(self::TITLE, $this->ws->getTitle());
+            $this->assertEquals(self::DESCRIPTION, $this->ws->getDescription());
+        }
+        catch (Exception $e) {
+            $this->fail('Exception occurred: '.$e->getMessage());
+        }
+    }
+
+    public function testExceptionEmptyArgs() {
+        try {
+            $this->ws = MetricsWorkspaceController::UpdateWorkspaceProfile('', '', '');
+        }
+        catch (Exception $e) {
+            return;
+        }
+
+        $this->fail('Expected exception calling method with no arguments', $e->getMessage());
+    }
+
+    public function testExceptionInexistentWorkspaceId() {
+        try {
+            $this->ws = MetricsWorkspaceController::UpdateWorkspaceProfile(
+                '999999', self::TITLE, self::DESCRIPTION
+            );
+        }
+        catch (Exception $e) {
+            return;
+        }
+
+        $this->fail('Inexistent PK WorkspaceId expects an exception');
+    }
+}
+?>
