bd7040da26f8739e786dfd9b2d44a2fabfc6a744
Andres Ardila <aardilam@gmail.com>
2008-11-22 07:26:08 +0000

    Modifying homepage to conform to Gary's requirements per his email of 08/21/08:
    "Your application start page (logon page) should include the following items:
    
    Indication that this is a SFSU/FAUFulda CS Software engineering project
    Group Number
    Group (or project) name, not ppm-x but the commercial name you have given your group
    Link back to your project home page on java.net
    Description of what your application does.
    How to start using the application"

--- app/index.php
+++ app/index.php
@@ -10,28 +10,65 @@
       
       <div id="content-wrap">
         <div id="inside">
-                                <div id="sidebar-right">
-              <div id="block-user-3" class="block block-user">
-  <h2>All users are welcomed</h2>
+          <div id="sidebar-right">
+            <div id="block-user-3" class="block block-user">
+              <h2>All users are welcomed</h2>
 
-  <div class="content" align="center">
+              <div class="content" align="center">
 
-<img src="/template/images/techglobe2.jpg">
+                <img src="/template/images/techglobe2.jpg">
 
-  </div>  </div>
-</div>
+              </div>
             </div>
-                    <div id="content">
-<BR>
-<div class="t"><div class="b"><div class="l"><div class="r"><div class="bl"><div class="br"><div class="tl"><div class="tr"><div class="content-in">
-
-Welcome to Infinity Metrics...
-
-    </div><br class="clear"></div></div></div></div></div></div></div></div>
           </div>
         </div>
+        <div id="content">
 
-          </div>
+          <br />
+
+          <div class="t"><div class="b"><div class="l"><div class="r"><div class="bl"><div class="br"><div class="tl"><div class="tr"><div class="content-in">
+
+            <table>
+                <tr>
+                    <td><h2>Welcome to Infinity Metrics</h2></td>
+                    <td align="right"><h4><a href="/user/">Get Started Now!</a></h4></td>
+                </tr>
+            </table>
+
+            <br />
+            <p>
+                <strong><em>Infinity Metrics</em></strong> is a distributed collaboration Software
+                Engineering project between students from San Francisco State University
+                (<a href="http://www.sfsu.edu">SFSU</a>) and Florida Atlantic University
+                (<a href="http://www.fau.edu">FAU</a>). Our project corresponds to <strong>Group 8</strong>
+                of the Project Participation Metrics project, or
+                <a href="https://ppm.dev.java.net">PPM</a>. The project aims to create
+                a web-based application that will enable instructors teaching
+                Software Engineering courses with distributed global development 
+                teams using the <a href="http://www.java.net">java.net</a> platform 
+                to collect and track student participation. To read more about
+                our project please visit its <a href="https://ppm-8.dev.java.net">java.net project space</a>.
+            </p>
+            <p>
+                Infinity Metrics tracks, collects and reports on student participation metrics
+                for such class environment mainly via the projects' syndicated RSS feeds on java.net.
+                The application allows a single instructor or set of instructors in different locations
+                (i.e. concurrent Software Engineering courses taking place in different
+                universities) to automatically collect participation data for a set of java.net projects and report on its
+                members’ contribution to the project at all stages of development.
+            <p>
+                We aim to provide an intuitive, easy-to-use application that requires little initial set-up
+                and will automatically generate the metrics categories available for the given projects,
+                whilst allowing instructors to retain full control to modify these settings
+                at any point throughout the semester.
+            </p>
+            <p>
+                To get started using the application, please visit the <a href="/user/">registration area</a>.
+            </p>
+          </div><br class="clear"></div></div></div></div></div></div></div></div>
+        </div>
+      </div>
+    </div>
 <?php
     include 'template/footer.php';
 ?>
