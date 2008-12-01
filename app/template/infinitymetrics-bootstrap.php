b5eabfa26a9faf3d1ed1bb93eb39129bdca2d52d
Marcello de Sales <marcello.desales@gmail.com>
2008-12-01 01:29:10 +0000

    Issue 77
    Updating the UI links with the correct values.

--- app/template/infinitymetrics-bootstrap.php
+++ app/template/infinitymetrics-bootstrap.php
@@ -31,6 +31,10 @@ function isUserInReservedAreas() {
                contains($_SERVER["REQUEST_URI"], "/report");
 }
 
+////ini_set("session.cache_expire","180"); // default is 180, which is 3 hours...
+ini_set("session.gc_maxlifetime","3600"); // default is 1440, which is only 24 minutes
+session_save_path("c:/ppm8-dev/sessions");
+
 session_start();
 if (isUserLoggedIn()) {
    //$user = $_SESSION["user"];
--- app/template/top-navigation-menu.php
+++ app/template/top-navigation-menu.php
@@ -10,37 +10,37 @@
                 !contains($_SERVER["REQUEST_URI"], "user") && !contains($_SERVER["REQUEST_URI"], "workspace")
                 ? "<li id=\"current\">" : "<li>";
 ?>
-                        <?php echo $liHome; ?><a href="<?php echo $home_address ?>">
-                                <img src="<?php echo $home_address ?>/template/edit_files/home.png" alt="" title="" width="32" height="32" border="0">
+                        <?php echo $liHome; ?><a href="<?php echo $_SERVER["home_address"] ?>">
+                                <img src="<?php echo $_SERVER["home_address"] ?>/template/edit_files/home.png" alt="" title="" width="32" height="32" border="0">
                                 <br>Home</a>
                         </li>
 <?php
 if (isUserLoggedIn()) {
 ?>
-                        <?php echo $liWorkspace; ?><a href="<?php echo $home_address ?>/workspace/workspaceCollection.php">
-                                <img src="<?php echo $home_address; ?>/template/edit_files/logo.png" alt="" title="" width="32" height="32" border="0">
+                        <?php echo $liWorkspace; ?><a href="<?php echo $_SERVER["home_address"] ?>/workspace/workspaceCollection.php">
+                                <img src="<?php echo $_SERVER["home_address"]; ?>/template/edit_files/logo.png" alt="" title="" width="32" height="32" border="0">
                                 <br>Workspace</a>
                         </li>
-                        <?php echo $liReports; ?><a href="<?php echo $home_address ?>/report">
-                                <img src="<?php echo $home_address; ?>/template/edit_files/admin-reports.png" alt="" title="" width="32" height="32" border="0">
+                        <?php echo $liReports; ?><a href="<?php echo $_SERVER["home_address"] ?>/report">
+                                <img src="<?php echo $_SERVER["home_address"]; ?>/template/edit_files/admin-reports.png" alt="" title="" width="32" height="32" border="0">
                                 <br>Reports</a>
                         </li>
-                        <?php echo $liCeTracker; ?><a href="<?php echo $home_address; ?>/cetracker">
-                                <img src="<?php echo $home_address; ?>/template/edit_files/node-add.png" alt="" title="" width="32" height="32" border="0">
+                        <?php echo $liCeTracker; ?><a href="<?php echo $_SERVER["home_address"]; ?>/cetracker">
+                                <img src="<?php echo $_SERVER["home_address"]; ?>/template/edit_files/node-add.png" alt="" title="" width="32" height="32" border="0">
                                 <br>Custom Events</a>
                         </li>
 <?php } ?>
-                        <?php echo $liUser; ?><a href="<?php echo $home_address ?>/user">
-                                <img src="<?php echo $home_address; ?>/template/edit_files/admin-user.png" alt="" title="" width="32" height="32" border="0">
+                        <?php echo $liUser; ?><a href="<?php echo $_SERVER["home_address"] ?>/user">
+                                <img src="<?php echo $_SERVER["home_address"]; ?>/template/edit_files/admin-user.png" alt="" title="" width="32" height="32" border="0">
                                 <br>Users</a>
                         </li>
 <?php if (isUserLoggedIn()) { ?>
-                        <?php echo $liConfig ?><a href="<?php echo $home_address ?>/config">
-                                <img src="<?php echo $home_address; ?>/template/edit_files/admin-settings.png" alt="" title="" width="32" height="32" border="0">
+                        <?php echo $liConfig ?><a href="<?php echo $_SERVER["home_address"] ?>/config">
+                                <img src="<?php echo $_SERVER["home_address"]; ?>/template/edit_files/admin-settings.png" alt="" title="" width="32" height="32" border="0">
                                 <br>Configuration</a>
                         </li>
 <?php } ?>
-                        <?php echo $liHelp ?><a href="<?php echo $home_address ?>/help">
-                                <img src="<?php echo $home_address; ?>/template/edit_files/admin-help.png" alt="" title="" width="32" height="32" border="0">
+                        <?php echo $liHelp ?><a href="<?php echo $_SERVER["home_address"] ?>/help">
+                                <img src="<?php echo $_SERVER["home_address"]; ?>/template/edit_files/admin-help.png" alt="" title="" width="32" height="32" border="0">
                                 <br>Help</a>
                         </li>
